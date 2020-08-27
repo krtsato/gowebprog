f94c7457c451064c6ea4eaa93d73eb9a818b2b25
krtsato <ev60596@meiji.ac.jp>
2020-08-27 23:30:11 +0900

    chore: add the description of handleFunc

--- ch03/08handlerfunc/server.go
+++ ch03/08handlerfunc/server.go
@@ -18,7 +18,14 @@ func main() {
 		Addr: "127.0.0.1:8080",
 		// Handlerは指定しない -> DefaultServeMuxをハンドラとして利用
 	}
-	http.HandleFunc("/hello", hello) // 関数をハンドラに変換して、DefaultServeMuxに登録
+
+	// 関数をハンドラに変換して、DefaultServeMux に登録
+	// ルーティングごとに発生する Handler の定義と
+	// レシーバー付き ServeHTTP の定義を省略できる
+
+	// i.e. ハンドラ関数 HandleFunc は
+	// 単に Handler を作成するショートカットのようなもの
+	http.HandleFunc("/hello", hello)
 	http.HandleFunc("/world", world)
 
 	server.ListenAndServe()
