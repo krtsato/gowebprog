700c9b87112fc9b8be483865c52552b327ab6195
krtsato <ev60596@meiji.ac.jp>
2020-08-29 23:45:22 +0900

    chore: add comments to cookie in golang

--- ch04/16cookie/server.go
+++ ch04/16cookie/server.go
@@ -6,6 +6,44 @@ import (
 	"net/http"
 )
 
+/*
+Go 言語における Cookie の構造体
+type Cookie struct {
+	Name string
+	Value string
+	Plan string
+	Domain string
+	Expires time.Time
+	RawExpires string
+	MaxAge int
+	Secure bool
+	HttpOnly bool
+	Raw string
+	Unparsed []string
+}
+
+Expires フィールドが設定されていない場合
+・セッションクッキー (一時的)
+・ブラウザが閉じられるとブラウザから削除される
+
+Expires フィールドが設定されている場合
+・期限切れ・削除されない限り持続する
+
+有効期限の指定方法
+・Expires フィールドを使う : いつ期限切れになるか
+・MaxAge フィールドを使う : ブラウザ内で生成されてから何秒間有効か
+
+Expires と MaxAge が混在している理由
+・ブラウザ間のクッキー実装の不一致のため
+・HTTP 1.1 では MaxAge が優先され Expires は非推奨
+・しかし Expires はほぼ全てのブラウザが未だにサポートしている
+・MaxAge は IE 6, 7 で未だにサポートされていない
+
+Expires と MaxAge どちらを使うか
+・Expires フィールドだけ使う
+・あらゆるブラウザをサポートするため両方使う
+*/
+
 func setCookie(w http.ResponseWriter, r *http.Request) {
 	c1 := http.Cookie{
 		Name:     "first_cookie",
