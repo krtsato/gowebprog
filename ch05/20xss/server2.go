c45bca11d470cc7020d6b72900b99dd51ea7a0f8
krtsato <ev60596@meiji.ac.jp>
2020-09-02 23:42:04 +0900

    chore: xss practice in the html template engine

--- ch05/20xss/server2.go
+++ ch05/20xss/server2.go
@@ -2,27 +2,30 @@
 package main
 
 import (
-  "net/http"
-  "html/template"
+	"html/template"
+	"net/http"
 )
 
-func process(w http.ResponseWriter, r *http.Request) {  
-//   w.Header().Set("X-XSS-Protection", "0") // 有効にするFirefox以外でも攻撃が可能になる
-  t, _ := template.ParseFiles("tmpl.html")  
-	//  t.Execute(w, r.FormValue("comment"))
-	t.Execute(w, template.HTML(r.FormValue("comment"))) // ← 
+func process(w http.ResponseWriter, r *http.Request) {
+	// ブラウザ標準の XSS 対策を無効にするために
+	// HTTP レスポンスヘッダである X-XSS-Protection を送信する
+	w.Header().Set("X-XSS-Protection", "0")
+	t, _ := template.ParseFiles("tmpl.html")
+
+	// HTML 型にキャストして Script タグを敢えて表示させる
+	t.Execute(w, template.HTML(r.FormValue("comment")))
 }
 
-func form(w http.ResponseWriter, r *http.Request) {  
-  t, _ := template.ParseFiles("form.html")  
-  t.Execute(w, nil)  
+func form(w http.ResponseWriter, r *http.Request) {
+	t, _ := template.ParseFiles("form.html")
+	t.Execute(w, nil)
 }
 
 func main() {
-  server := http.Server{
-    Addr: "127.0.0.1:8080",
-  }
-  http.HandleFunc("/process", process)
-  http.HandleFunc("/", form)
-  server.ListenAndServe()
+	server := http.Server{
+		Addr: "127.0.0.1:8080",
+	}
+	http.HandleFunc("/process", process)
+	http.HandleFunc("/", form)
+	server.ListenAndServe()
 }
